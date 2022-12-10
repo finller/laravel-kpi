afdb995947f13706e46595de78362b84a5f9a17f
QuentinGab <QuentinGab@users.noreply.github.com>
2022-12-10 13:46:56 +0000

    Fix styling

--- database/factories/KpiFactory.php
+++ database/factories/KpiFactory.php
@@ -5,7 +5,6 @@ namespace Finller\Kpi\Database\Factories;
 use Finller\Kpi\Kpi;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-
 class KpiFactory extends Factory
 {
     protected $model = Kpi::class;
