d814b5da8c79d0f9602ce46087723c0197965405
Quentin Gabriele <quentin.gabriele@polyconseil.fr>
2023-02-16 12:52:25 +0100

    stan is happy with laravel 10

--- src/Adapters/AbstractAdapter.php
+++ src/Adapters/AbstractAdapter.php
@@ -4,5 +4,5 @@ namespace Finller\Kpi\Adapters;
 
 abstract class AbstractAdapter
 {
-    abstract public function groupBy(string $column, string $interval): \Illuminate\Database\Query\Expression;
+    abstract public function groupBy(string $column, string $interval): \Illuminate\Contracts\Database\Query\Expression;
 }
